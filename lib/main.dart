9b88445e5d7c12a67d4e252d7c640649394ca7f6
Mikailov05 <ahmed.mikailov@interner.ru>
2026-04-08 18:35:37 +0300

    completed task 3 and completed labs

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,32 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Привет! Меня зовут Ахмед.\nЯ студент группы ИСП-231',
-          style: const TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          
+            Image.network(
+              'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/8dd2b230-813b-4990-b615-6e6137944d43/3840x',
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Text(
+                  ' Картинка не загрузилась',
+                  style: TextStyle(color: Colors.red, fontSize: 16),
+                );
+              },
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'Привет! Меня зовут Ахмед.\nЯ студент группы ИСП-231',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
